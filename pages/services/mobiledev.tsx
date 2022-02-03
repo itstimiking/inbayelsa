01de49c388915ce57abd46a26b9b6c9e7f64b29c
itstimiking <26443032+itstimiking@users.noreply.github.com>
2022-02-04 00:36:29 +0100

    color change

--- pages/services/mobiledev.tsx
+++ pages/services/mobiledev.tsx
@@ -4,17 +4,17 @@ const Mobiledev = () => {
     return (
         <div>
 
-            <P>
-            Our offshore team has decades of experience in custom mobile app development that spans various industries from retail to healthcare and energy. OpenXcell’s proactive outsourcing team can dive into your business ecosystem, explore the market, and understand your needs, requirements, and capabilities. Then, you can choose the offshore team of your choice to build a custom strategy and unlock mobile technology’s full potential for your company. It’s what custom mobile app development is all about where our team gets to know your business and tailor the application development services as per your goals and expectations.
+            <p>
+                Our offshore team has decades of experience in custom mobile app development that spans various industries from retail to healthcare and energy. OpenXcell’s proactive outsourcing team can dive into your business ecosystem, explore the market, and understand your needs, requirements, and capabilities. Then, you can choose the offshore team of your choice to build a custom strategy and unlock mobile technology’s full potential for your company. It’s what custom mobile app development is all about where our team gets to know your business and tailor the application development services as per your goals and expectations.
 
  
 
-Right from wireframing to prototyping the MVP, our offshore team works closely with you to incorporate your invaluable inputs. On-demand solutions, economical white label products, industry-specific apps, and the list goes on.
+                Right from wireframing to prototyping the MVP, our offshore team works closely with you to incorporate your invaluable inputs. On-demand solutions, economical white label products, industry-specific apps, and the list goes on.
 
  
 
-Our highly experienced and dedicated offshore developers will provide you with full-cycle mobile app development services, including expert business analysis, design, and development of the mobile application, right from idea to launch, integrating the new product into the infrastructure, and further providing further development scale-up and optimization on demand. Our team is filled with experienced front-end, back-end, and cloud developers and infrastructure to keep up with technical advances. 
-            </P>
+                Our highly experienced and dedicated offshore developers will provide you with full-cycle mobile app development services, including expert business analysis, design, and development of the mobile application, right from idea to launch, integrating the new product into the infrastructure, and further providing further development scale-up and optimization on demand. Our team is filled with experienced front-end, back-end, and cloud developers and infrastructure to keep up with technical advances. 
+            </p>
 
             <div>
                 <h1>Stages RoadMap</h1>
